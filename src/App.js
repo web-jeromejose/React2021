e87a351f4435200018552b40412c496a897ac21b
Jerome <mesekarome@gmail.com>
2022-01-10 20:10:33 +0000

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+ 
+import { Button } from 'react-bootstrap';
+
+function App() {
+    return ( 
+        <div>
+            <Button variant="link" className="btn btn-primary" >
+        Posts
+      </Button>
+        </div>
+    );
+}
+
+export default App;
